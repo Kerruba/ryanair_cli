2a2e65b8673ce7b9d6108cdc47b3a45fcdbad18c
Luca Cherubin <luca.cherubin@gmail.com>
2018-05-07 11:19:00 +0200

    Add utility library and create tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,40 @@
-var assert = require('chai').assert;
+let should = require('chai').should();
+let utilities = require('../lib/utilities.js')
 
 describe('Options', function() {
-    describe('#time options', function() {
-        it('should accept gt departure filter', function() {
-            
+    describe('#timeFilters', function() {
+        it('should convert >16:00 to timeFilter object', function() {
+            let filterObject = utilities.timeFilter('>16:00');
+            filterObject.should.be.deep.equal({'method': '>', 'time': '16:00'})
         })
+
+       it('should generate an error for invalid time filter method', function() {
+           (function() {
+               utilities.timeFilter("><16:00");
+           }).should.throw();
+       })
+
+       it('should generate an error for invalid time filter value', function() {
+           (function() {
+               utilities.timeFilter(">16.00");
+           }).should.throw();
+       })
+    })
+
+    describe("#priceFilters", function() {
+        it('should convert >16.00 to timeFilter object', function() {
+            let filterObject = utilities.priceFilter('==16.00');
+            filterObject.should.be.deep.equal({'method': '==', 'price': 16})
+        })
+
+        it('should throw an error for invalid method compare method', function() {
+            (function() {utilities.priceFilter('<>16.00') }).should.throw();
+        })
+
+        it('should throw an error for invalid price format', function() {
+            (function() {utilities.priceFilter('==16..00') }).should.throw();
+        })
+
     })
 
 })
