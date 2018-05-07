b4da8524545fb68f762a97752ad6d1773545576c
Luca Cherubin <luca.cherubin@gmail.com>
2018-05-07 09:43:29 +0200

    Change file structure

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var assert = require('chai').assert;
+
+describe('Options', function() {
+    describe('#time options', function() {
+        it('should accept gt departure filter', function() {
+            
+        })
+    })
+
+})
+
+describe('Search', function() {
+
+})
+
+describe('Content', function() {
+    
+})
